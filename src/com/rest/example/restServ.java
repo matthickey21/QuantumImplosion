b6ddadf384a38a5cec9fa95db96d299e2f1ff9fa
ETHAN <ethan@eduroamguest-2-072.sys.cs.local>
2017-04-12 13:47:52 -0400

    Added everything in directory structure

---
+++ src/com/rest/example/restServ.java
@@ -0,0 +1,66 @@
+package com.rest.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+@Path("/rest")
+public class restServ {
+  private static final String dbHost = "67.205.170.157";
+  private static final int dbPort = 5432;
+  private static final String dbName = "quantumimplosion";
+  private static final String dbUsername = "quantumimplosion";
+  private static final String dbPassword = "notpassword";
+  private static Connection connection;
+
+ @GET
+ //-------this might be the issue -------------
+ @Produces(MediaType.TEXT_PLAIN)
+ public void getTest() throws SQLException{
+		PreparedStatement select = connection.prepareCall("SELECT * FROM test_table");
+        ResultSet selectResults = select.executeQuery();
+ }
+
+ private static boolean connect()
+ {
+     if (connection == null)
+     {
+         try
+         {
+             connection = DriverManager.getConnection(
+                        String.format("jdbc:postgresql://%s:%d/%s", dbHost, dbPort, dbName),dbUsername, dbPassword);
+             //                connection = new PgConnection(dbHost);
+         }
+         catch (Exception e)
+         {
+             return false;
+         }
+     }
+     return true;
+ }    private static void close()
+ {
+     if (connection != null)
+     {
+         try {
+             connection.close();
+         } catch (SQLException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+     }
+ }
+
+
+
+
+}
