5bd1725f042a579d6c6e5c52275bd5e688efb580
Anandhiya Baratham <abaratham@1x-nat-vl930-172-30-64-245.wireless.umass.edu>
2017-04-25 19:34:50 -0400

    move one more table entry

--- CSVParser/src/main/com/quantumimplosion/parser/CSVParserConstants.java
+++ CSVParser/src/main/com/quantumimplosion/parser/CSVParserConstants.java
@@ -29,10 +29,10 @@ public class CSVParserConstants {
 	{
 		Map<String, String> temp2 = new HashMap<String, String>();
 		temp2.put("node_information", "INSERT INTO node_information (system_id, from, to, nodeUpSinceMostRecent, nodeCountOffline, nodeCountMissing) VALUES (?, ?, ?, ?, ?, ?);");
-		temp2.put("system", "INSERT INTO system (system_id, system_companyName, system_model, updated, system_install_date, capacity_total_sizeTiB, capacity_total_freePct, capacity_byType_fc_sizeTiB, capacity_byType_nl_sizeTiB, capacity_byType_ssd_sizeTiB, virtualCapacity_byType_tdvv_vvCount, virtualCapacity_byType_tdvv_sizeTiB) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+		temp2.put("system", "INSERT INTO system (system_id, system_companyName, system_model, updated, system_install_date, capacity_total_sizeTiB, capacity_total_freePct, capacity_byType_fc_sizeTiB, capacity_byType_nl_sizeTiB, capacity_byType_ssd_sizeTiB, virtualCapacity_byType_tdvv_vvCount, virtualCapacity_byType_tdvv_sizeTiB, capacity_total_dedupeRatio) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
 		temp2.put("file_io", "INSERT INTO file_io (system_id, from, to, totalWriteIOsHistVlun, totalWriteIOsHistPortTargets, delAcks, delAcksPct, writesGt16s, writesGtXmsPct, writes1msPct, readsGtXmsPct, readsXmsPct, totalsGtXmsPct, totalsXmsPct, portReadAvgIOSizeKB, portWriteAvgIOSizeKB, portTotalAvgIOSizeKB) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
 		temp2.put("bandwidth", "INSERT INTO bandwidth (system_id, from, to, portReadBandwidthMBPS, portWriteBandwidthMBPS, portTotalBandwidthMBPS) VALUES (?, ?, ?, ?, ?, ?);");
-		temp2.put("deduplication", "INSERT INTO deduplication(system_id, from, to, ddsSizeUsedTiB , ddsSizeUsedTiBPrevious, capacity_total_dedupeRatio) VALUES (?, ?, ?, ?, ?, ?);");
+		temp2.put("deduplication", "INSERT INTO deduplication(system_id, from, to, ddsSizeUsedTiB , ddsSizeUsedTiBPrevious) VALUES (?, ?, ?, ?, ?);");
 		temp2.put("storage_usage", "INSERT INTO storage_usage (system_id, from, to, vvCountHistVlun, vvCountHistVlunPrevious) VALUES (?, ?, ?, ?, ?);");
 		temp2.put("cpu_usage", "INSERT INTO cpu_usage (system_id, from, to, cpuLatestSysAvgPct, cpuLatestUserAvgPct, cpuLatestTotalAvgPct, cpuLatestSysMaxPct, cpuLatestUserMaxPct, cpuLatestTotalMaxPct) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
 
@@ -44,10 +44,10 @@ public class CSVParserConstants {
 	{
 		Map<String, List<String>> temp3 = new HashMap<String, List<String>>();
 		temp3.put("node_information", Arrays.asList("systemId", "from", "to", "nodeUpSinceMostRecent", "nodeCountOffline", "nodeCountMissing"));
-		temp3.put("system", Arrays.asList("systemId", "system_companyName", "system_model", "updated", "system_install_date", "capacity_total_sizeTiB", "capacity_total_freePct", "capacity_byType_fc_sizeTiB", "capacity_byType_nl_sizeTiB", "capacity_byType_ssd_sizeTiB", "virtualCapacity_byType_tdvv_vvCount", "virtualCapacity_byType_tdvv_sizeTiB"));
+		temp3.put("system", Arrays.asList("systemId", "system_companyName", "system_model", "updated", "system_install_date", "capacity_total_sizeTiB", "capacity_total_freePct", "capacity_byType_fc_sizeTiB", "capacity_byType_nl_sizeTiB", "capacity_byType_ssd_sizeTiB", "virtualCapacity_byType_tdvv_vvCount", "virtualCapacity_byType_tdvv_sizeTiB", "capacity_total_dedupeRatio"));
 		temp3.put("file_io", Arrays.asList("systemId", "from", "to", "totalWriteIOsHistVlun", "totalWriteIOsHistPortTargets", "delAcks", "delAcksPct", "writesGt16s", "writesGt32msPct", "writes1msPct", "readsGt32msPct", "reads32msPct", "totalsGt32msPct", "totals32msPct", "portReadAvgIOSizeKB", "portWriteAvgIOSizeKB", "portTotalAvgIOSizeKB"));
 		temp3.put("bandwidth", Arrays.asList("systemId", "from", "to", "portReadBandwidthMBPS", "portWriteBandwidthMBPS", "portTotalBandwidthMBPS"));
-		temp3.put("deduplication", Arrays.asList("systemId", "from", "to", "ddsSizeUsedTiB", "ddsSizeUsedTiBPrevious", "capacity_total_dedupeRatio"));
+		temp3.put("deduplication", Arrays.asList("systemId", "from", "to", "ddsSizeUsedTiB", "ddsSizeUsedTiBPrevious"));
 		temp3.put("storage_usage", Arrays.asList("systemId", "from", "to", "vvCountHistVlun", "vvCountHistVlunPrevious"));
 		temp3.put("cpu_usage", Arrays.asList("systemId", "from", "to", "cpuLatestSysAvgPct", "cpuLatestUserAvgPct", "cpuLatestTotalAvgPct", "cpuLatestSysMaxPct", "cpuLatestUserMaxPct", "cpuLatestTotalMaxPct"));
 		tableToHeadersMap = Collections.unmodifiableMap(temp3);
