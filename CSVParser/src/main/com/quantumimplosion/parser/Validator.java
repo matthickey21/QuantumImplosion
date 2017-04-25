2ce96943bf0f9fa45e0cc58552e261f948eab788
Anandhiya Baratham <abaratham@1x-nat-vl930-172-30-64-245.wireless.umass.edu>
2017-04-25 19:44:17 -0400

    Add temporary invalid columns for validateSummary since I'm too lazy to actually write the expected types

--- CSVParser/src/main/com/quantumimplosion/parser/Validator.java
+++ CSVParser/src/main/com/quantumimplosion/parser/Validator.java
@@ -10,7 +10,7 @@ import org.apache.commons.csv.CSVRecord;
 public class Validator {
 
 	private static final ArrayList<Class<?>> expectedPerformanceTypes;
-
+	
 	//This is terrible, I know. Sue me
 	static {
 		//Hardcoded column types
@@ -35,7 +35,7 @@ public class Validator {
 
 	public static Set<Integer> validateSummaryRecord(CSVRecord record)
 	{
-		return null;
+		return new HashSet<Integer>();
 	}
 
 	public static Set<Integer> validatePerformanceRecord(CSVRecord record)
