c0fd9ca51f4ef2a516c465b4d7b2a0c1f2efbd00
jakecelentano <jcelentano@umass.edu>
2017-04-30 21:57:35 -0400

    Update restServ.java

--- src/com/rest/example/restServ.java
+++ src/com/rest/example/restServ.java
@@ -25,7 +25,6 @@ public class restServ {
 
   @GET
  @Produces(MediaType.WILDCARD)
- 
  public ResultSet getTest() throws SQLException{
 	 	connect();
 		PreparedStatement select = connection.prepareCall("SELECT * FROM test_table");
@@ -39,6 +38,8 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/{user}/sets")
  public ResultSet retrieveSystemSets(@PathParam("user") String user){
+	 connect();
+	 close();
 	return null;
 	 
  }
@@ -46,12 +47,16 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/{user}/systems")
  public ResultSet retrieveSystem(@PathParam("user") String user) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @POST
  @Path("/{user}/sets/create")
  public void createSystemSet(@PathParam("user") String user, String setName, List<String> systemList){
+	 connect();
+	 close();
 	 
  }
  
@@ -59,13 +64,17 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}")
  public ResultSet systemOverview(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}")
- public ResultSet systemSetOverview(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetOverview(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -73,13 +82,17 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/storage")
  public ResultSet systemStorageData(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}/storage")
- public ResultSet systemSetStorageData(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetStorageData(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -87,13 +100,17 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/cpu")
  public ResultSet systemCPUdata(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}/cpu")
- public ResultSet systemSetCPUdata(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetCPUdata(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -101,13 +118,17 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/io")
  public ResultSet systemFileIOdata(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}/io")
- public ResultSet systemSetFileIOdata(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetFileIOdata(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -115,6 +136,8 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/bandwidth")
  public ResultSet systemBandwidthData(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -122,6 +145,8 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/node")
  public ResultSet systemNodeInfo(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -129,7 +154,9 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}/node")
- public ResultSet systemSetNodeInfo(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetNodeInfo(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -137,13 +164,17 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/dedupe")
  public ResultSet systemDeduplicationData(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set}/dedupe")
- public ResultSet systemSetDeduplicationData(@PathParam("system_set") String system_set) {
+ public ResultSet systemSetDeduplicationData(@PathParam("system_set") List<String> system_set) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -151,6 +182,8 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/compare")
  public ResultSet likeSystemComparison(@PathParam("system") String system) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
@@ -159,17 +192,34 @@ public class restServ {
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system_1}/compare/{system_2}")
  public ResultSet specificSystemComparison(@PathParam("system_1") String system_1, @PathParam("system_2") String system_2) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/sets/{system_set_2}/compare/{system_set_2}")
- public ResultSet specificSystemSetComparison(@PathParam("system_1") String system_1, @PathParam("system_2") String system_2) {
+ public ResultSet specificSystemSetComparison(@PathParam("system_set_1") List<String> system_set_1, @PathParam("system_set_2") List<String> system_set_2) {
+	 connect();
+	 close();
 	 return null;
 	 
  }
 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  private static boolean connect()
  {
      if (connection == null)
