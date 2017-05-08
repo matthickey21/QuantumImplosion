e14a1479a3a645f3547de8cb0c13f651d8cfe6b3
mtrifonov <mtrifonov@umass.edu>
2017-05-07 21:24:24 -0400

    implemented essential restServ api methods

--- src/com/rest/example/restServ.java
+++ src/com/rest/example/restServ.java
@@ -1,4 +1,5 @@
 package com.rest.example;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,56 +15,65 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
-import java.net.ConnectionException;
+import java.net.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 @Path("/rest")
 public class restServ {
   private static final String dbHost = "67.205.170.157";
   private static final int dbPort = 5432;
-  private static final String dbName = "quantumimplosion";
+  private static final String dbName = "overload";
   private static final String dbUsername = "quantumimplosion";
   private static final String dbPassword = "notpassword";
-  private static Connection connection;
+  private static Connection connection = null;
 
   @GET
- @Produces(MediaType.WILDCARD)
- public ResultSet getTest() throws SQLException{
-	 	connect();
-		PreparedStatement select = connection.prepareCall("SELECT * FROM test_table");
+ @Produces(MediaType.APPLICATION_JSON)
+ public JSONArray getTest() throws SQLException, JSONException{
+	  	connect();
+		PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"System\"");
         ResultSet selectResults = select.executeQuery();
-        while(selectResults.next()){
-        	System.out.println(selectResults.getString(1));
-        }
         close();
-		return selectResults;
+		return convert(selectResults);
  }
  
  
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/{user}/sets")
- public ResultSet retrieveSystemSets(@PathParam("user") String user) throws ConnectionException{
+ public ResultSet retrieveSystemSets(@PathParam("user") String user) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: retrieveSystemSets");
-	 close();
-	return null;
+		 	throw new ConnectException("Cannot connect to database: retrieveSystemSets");
+		 PreparedStatement select = connection.prepareCall("SELECT set_id FROM public.\"System Set\" WHERE company = '" + user + "';");
+	     ResultSet selectResults = select.executeQuery();
+		 close();
+		 return selectResults;
  }
 
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/{user}/systems")
- public ResultSet retrieveSystem(@PathParam("user") String user) throws ConnectionException {
+ public ResultSet retrieveSystem(@PathParam("user") String user) throws ConnectException, SQLException {
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: retrieveSystem");
-	 close();
-	 return null;
-	 
+		 	throw new ConnectException("Cannot connect to database: retrieveSystem");
+		 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"System\" WHERE \"system_companyName\" = '" + user + "';");
+	     ResultSet selectResults = select.executeQuery();
+		 close();
+		 return selectResults; 
  }
+ 
  @POST
  @Path("/{user}/sets/create")
  public void createSystemSet(@PathParam("user") String user, String setName, List<String> systemList) throws ConnectionException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: createSystemSet");
+	 	throw new ConnectException("Cannot connect to database: createSystemSet");
 	 Statement statement = connection.createStatement();
 	 close();
 	 
@@ -72,11 +82,13 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}")
- public ResultSet systemOverview(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemOverview(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemOverview");
+	 	throw new ConnectException("Cannot connect to database: systemOverview");
+	 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"System\" WHERE system_id = " + system + ";");
+     ResultSet selectResults = select.executeQuery();
 	 close();
-	 return null;
+	 return selectResults;
 	 
  }
  @GET
@@ -92,11 +104,13 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/storage")
- public ResultSet systemStorageData(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemStorageData(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemStorageData");
-	 close();
-	 return null;
+		 	throw new ConnectException("Cannot connect to database: systemStorageData");
+		 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"Storage Usage\" WHERE system_id = " + system + ";");
+	     ResultSet selectResults = select.executeQuery();
+		 close();
+		 return selectResults;
 	 
  }
  @GET
@@ -112,11 +126,13 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/cpu")
- public ResultSet systemCPUdata(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemCPUdata(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemCPUdata");
-	 close();
-	 return null;
+		 	throw new ConnectException("Cannot connect to database: systemCPUdata");
+		 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"CPU Usage\" WHERE system_id = " + system + ";");
+	     ResultSet selectResults = select.executeQuery();
+		 close();
+		 return selectResults;
 	 
  }
  @GET
@@ -132,11 +148,13 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/io")
- public ResultSet systemFileIOdata(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemFileIOdata(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemFileIOdata");
-	 close();
-	 return null;
+		 	throw new ConnectException("Cannot connect to database: systemOverview");
+		 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"File I/O\" WHERE system_id = " + system + ";");
+	     ResultSet selectResults = select.executeQuery();
+		 close();
+		 return selectResults;
 	 
  }
  @GET
@@ -251,8 +269,8 @@ public class restServ {
      {
          try
          {
-             connection = DriverManager.getConnection(
-                        String.format("jdbc:postgresql://%s:%d/%s", dbHost, dbPort, dbName),dbUsername, dbPassword);
+        	 connection = DriverManager.getConnection(
+                     String.format("jdbc:postgresql://%s:%d/%s", dbHost, dbPort, dbName),dbUsername, dbPassword);
              //                connection = new PgConnection(dbHost);
          }
          catch (Exception e)
@@ -276,7 +294,68 @@ public class restServ {
      }
  }
 
+ public static JSONArray convert( ResultSet rs )
+		    throws SQLException, JSONException
+		  {
+		    JSONArray json = new JSONArray();
+		    ResultSetMetaData rsmd = rs.getMetaData();
 
+		    while(rs.next()) {
+		      int numColumns = rsmd.getColumnCount();
+		      JSONObject obj = new JSONObject();
+
+		      for (int i=1; i<numColumns+1; i++) {
+		        String column_name = rsmd.getColumnName(i);
+
+		        if(rsmd.getColumnType(i)==java.sql.Types.ARRAY){
+		         obj.put(column_name, rs.getArray(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.BIGINT){
+		         obj.put(column_name, rs.getInt(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.BOOLEAN){
+		         obj.put(column_name, rs.getBoolean(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.BLOB){
+		         obj.put(column_name, rs.getBlob(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.DOUBLE){
+		         obj.put(column_name, rs.getDouble(column_name)); 
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.FLOAT){
+		         obj.put(column_name, rs.getFloat(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.INTEGER){
+		         obj.put(column_name, rs.getInt(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.NVARCHAR){
+		         obj.put(column_name, rs.getNString(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.VARCHAR){
+		         obj.put(column_name, rs.getString(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.TINYINT){
+		         obj.put(column_name, rs.getInt(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.SMALLINT){
+		         obj.put(column_name, rs.getInt(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.DATE){
+		         obj.put(column_name, rs.getDate(column_name));
+		        }
+		        else if(rsmd.getColumnType(i)==java.sql.Types.TIMESTAMP){
+		        obj.put(column_name, rs.getTimestamp(column_name));   
+		        }
+		        else{
+		         obj.put(column_name, rs.getObject(column_name));
+		        }
+		      }
+
+		      json.put(obj);
+		    }
+
+		    return json;
+		  }
 
 
 }
