5d38541fdf751a794ce42b95b0d95534b40154d8
mtrifonov <mtrifonov@umass.edu>
2017-05-09 08:13:32 -0400

    implemented everything necessary to pull data into the UI

--- src/com/rest/example/restServ.java
+++ src/com/rest/example/restServ.java
@@ -1,49 +1,28 @@
 package com.rest.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Collection;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-
-import java.net.*;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
+	import java.sql.Connection;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import javax.ws.rs.GET;
+	import javax.ws.rs.Path;
+	import javax.ws.rs.PathParam;
+	import javax.ws.rs.Produces;
+	import javax.ws.rs.core.MediaType;
+	import java.net.*;
+	import org.apache.tomcat.jdbc.pool.DataSource;
+	import org.apache.tomcat.jdbc.pool.PoolProperties;
+	import org.json.JSONArray;
+	import org.json.JSONException;
+	import org.json.JSONObject;
+	import java.sql.ResultSetMetaData;
 
 @Path("/rest")
 public class restServ {
-  private static final String dbHost = "67.205.170.157";
-  private static final int dbPort = 5432;
-  private static final String dbName = "overload";
-  private static final String dbUsername = "quantumimplosion";
-  private static final String dbPassword = "notpassword";
-  private static Connection connection = null;
 
-  @GET
- @Produces(MediaType.APPLICATION_JSON)
- public JSONArray getTest() throws SQLException, JSONException{
-	  	connect();
-		PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"System\"");
-        ResultSet selectResults = select.executeQuery();
-        close();
-		return convert(selectResults);
- }
- 
+  private static Connection connection = null;
+  static PoolProperties p = null;
+  static DataSource s = null;
  
  @GET
  @Produces(MediaType.WILDCARD)
@@ -69,60 +48,32 @@ public class restServ {
 		 return selectResults; 
  }
  
- @POST
- @Path("/{user}/sets/create")
- public void createSystemSet(@PathParam("user") String user, String setName, List<String> systemList) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectException("Cannot connect to database: createSystemSet");
-	 Statement statement = connection.createStatement();
-	 close();
-	 
- }
- 
  @GET
- @Produces(MediaType.WILDCARD)
+ @Produces(MediaType.APPLICATION_JSON)
  @Path("/data/{system}")
- public ResultSet systemOverview(@PathParam("system") String system) throws ConnectException, SQLException{
+ public String systemOverview(@PathParam("system") String system) throws ConnectException, SQLException, JSONException{
 	 if(connect() == false)
 	 	throw new ConnectException("Cannot connect to database: systemOverview");
 	 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"System\" WHERE system_id = " + system + ";");
      ResultSet selectResults = select.executeQuery();
 	 close();
-	 return selectResults;
+	 return convert(selectResults).toString();
 	 
  }
+ 
  @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}")
- public ResultSet systemSetOverview(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetOverview");
-	 close();
-	 return null;
-	 
- }
- @GET
- @Produces(MediaType.WILDCARD)
+ @Produces(MediaType.APPLICATION_JSON)
  @Path("/data/{system}/storage")
- public ResultSet systemStorageData(@PathParam("system") String system) throws ConnectException, SQLException{
+ public String systemStorageData(@PathParam("system") String system) throws ConnectException, SQLException, JSONException{
 	 if(connect() == false)
 		 	throw new ConnectException("Cannot connect to database: systemStorageData");
 		 PreparedStatement select = connection.prepareCall("SELECT * FROM public.\"Storage Usage\" WHERE system_id = " + system + ";");
 	     ResultSet selectResults = select.executeQuery();
 		 close();
-		 return selectResults;
+		 return convert(selectResults).toString();
 	 
  }
- @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}/storage")
- public ResultSet systemSetStorageData(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetStorageData");
-	 close();
-	 return null;
-	 
- }
+ 
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/cpu")
@@ -135,16 +86,7 @@ public class restServ {
 		 return selectResults;
 	 
  }
- @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}/cpu")
- public ResultSet systemSetCPUdata(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetCPUdata");
-	 close();
-	 return null;
-	 
- }
+
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/io")
@@ -157,22 +99,13 @@ public class restServ {
 		 return selectResults;
 	 
  }
- @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}/io")
- public ResultSet systemSetFileIOdata(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetFileIOdata");
-	 close();
-	 return null;
-	 
- }
+
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/bandwidth")
- public ResultSet systemBandwidthData(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemBandwidthData(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemBandwidthData");
+	 	throw new ConnectException("Cannot connect to database: systemBandwidthData");
 	 close();
 	 return null;
 	 
@@ -180,9 +113,9 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/node")
- public ResultSet systemNodeInfo(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemNodeInfo(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemNodeInfo");
+	 	throw new ConnectException("Cannot connect to database: systemNodeInfo");
 	 close();
 	 return null;
 	 
@@ -190,40 +123,21 @@ public class restServ {
 
  @GET
  @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}/node")
- public ResultSet systemSetNodeInfo(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetNodeInfo");
-	 close();
-	 return null;
-	 
- }
- @GET
- @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/dedupe")
- public ResultSet systemDeduplicationData(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet systemDeduplicationData(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemDeduplicationData");
+	 	throw new ConnectException("Cannot connect to database: systemDeduplicationData");
 	 close();
 	 return null;
 	 
  }
- @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set}/dedupe")
- public ResultSet systemSetDeduplicationData(@PathParam("system_set") List<String> system_set) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: systemSetDeduplicationData");
-	 close();
-	 return null;
-	 
- }
+
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system}/compare")
- public ResultSet likeSystemComparison(@PathParam("system") String system) throws ConnectionException{
+ public ResultSet likeSystemComparison(@PathParam("system") String system) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: likeSystemComparison");
+	 	throw new ConnectException("Cannot connect to database: likeSystemComparison");
 	 close();
 	 return null;
 	 
@@ -232,53 +146,25 @@ public class restServ {
  @GET
  @Produces(MediaType.WILDCARD)
  @Path("/data/{system_1}/compare/{system_2}")
- public ResultSet specificSystemComparison(@PathParam("system_1") String system_1, @PathParam("system_2") String system_2) throws ConnectionException{
+ public ResultSet specificSystemComparison(@PathParam("system_1") String system_1, @PathParam("system_2") String system_2) throws ConnectException, SQLException{
 	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: specificSystemComparison");
+	 	throw new ConnectException("Cannot connect to database: specificSystemComparison");
 	 close();
 	 return null;
 	 
  }
- @GET
- @Produces(MediaType.WILDCARD)
- @Path("/data/sets/{system_set_2}/compare/{system_set_2}")
- public ResultSet specificSystemSetComparison(@PathParam("system_set_1") List<String> system_set_1, @PathParam("system_set_2") List<String> system_set_2) throws ConnectionException{
-	 if(connect() == false)
-	 	throw new ConnectionException("Cannot connect to database: specificSystemSetComparison");
-	 close();
-	 return null;
-	 
- }
-
  
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- private static boolean connect()
+ private static boolean connect() throws SQLException
  {
-     if (connection == null)
-     {
-         try
-         {
-        	 connection = DriverManager.getConnection(
-                     String.format("jdbc:postgresql://%s:%d/%s", dbHost, dbPort, dbName),dbUsername, dbPassword);
-             //                connection = new PgConnection(dbHost);
-         }
-         catch (Exception e)
-         {
-             return false;
-         }
-     }
-     return true;
+	 p = new PoolProperties();
+		p.setUrl("jdbc:postgresql://67.205.170.157:5432/" + "overload" );
+		p.setDriverClassName( "org.postgresql.Driver" );
+		p.setUsername( "quantumimplosion" );
+		p.setPassword( "notpassword" );
+		s = new org.apache.tomcat.jdbc.pool.DataSource( p );
+		s.setPoolProperties(p);
+		connection = s.getConnection();
+		return true;
  }   
 
  private static void close()
